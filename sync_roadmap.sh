4a8898ad9bfbee057199acf6894ad706ada1f7c4
briley <brian.riley@ucop.edu>
2018-05-22 15:22:38 -0700

    updated roadmap sync script to work against combined-ruby-notification-versioning

--- sync_roadmap.sh
+++ sync_roadmap.sh
@@ -2,14 +2,14 @@ if ! git ls-remote roadmap; then
   echo "No remote found for DMPRoadmap/roadmap ... adding one now"
   git remote add roadmap https://github.com/DMPRoadmap/roadmap.git
 fi
-git fetch origin development
-echo "Fetching latest roadmap branch"
-git fetch origin roadmap
-git checkout roadmap
+git fetch origin combined
+echo "Fetching latest roadmap combined-ruby-notification-versioning"
+git fetch origin combined-ruby-notification-versioning
+git checkout combined-ruby-notification-versioning
 echo "Pulling down latest changes from DMPRoadmap"
 #git pull roadmap master
-git pull roadmap development
-echo "Pushing updated branch"
-git push origin roadmap
-git checkout development
-echo "Switching to the development branch"
+git pull roadmap combined-ruby-notification-versioning
+#echo "Pushing updated branch"
+#git push origin combined-ruby-notification-versioning
+git checkout combined
+echo "Switching to the combined branch"
